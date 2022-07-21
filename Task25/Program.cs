71685735955f2437b7621b43f29d68918e6ea735
Alexey Maslov <alvladigital@gmail.com>
2022-07-21 23:33:43 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+//в натуральную степень B
+
+void Solution1()
+
+{
+Console.WriteLine("Insert number A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert number B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+long result = numberA;
+int count = 1;
+while(count < numberB)
+{
+    result *= numberA;
+    count++;
+}
+
+Console.WriteLine($"{numberA}^{numberB}={result}");
+}
+
+
+
+void Solution2()
+
+{
+Console.WriteLine("Insert number A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert number B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+long result = 1;
+for (int i = 1; i <= numberB; i++)
+{
+    result *= numberA;
+}
+
+Console.WriteLine($"{numberA}^{numberB}={result}");
+}
+
+Solution1();
+Solution2();
