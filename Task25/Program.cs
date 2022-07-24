2c17fc6ee1dc8bc42af6945d3f99af3c83b07fca
Alexey Maslov <alvladigital@gmail.com>
2022-07-24 11:38:56 +0300

    add modified Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -4,39 +4,50 @@
 void Solution1()
 
 {
-Console.WriteLine("Insert number A:");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Insert number B:");
-int numberB = Convert.ToInt32(Console.ReadLine());
-long result = numberA;
-int count = 1;
-while(count < numberB)
-{
-    result *= numberA;
-    count++;
-}
+    Console.WriteLine("Insert number A:");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Insert a natural number B:");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    if (numberB < 0)
+    {
+        Console.WriteLine("The number B does't natural");
+    }
+    else
+    {
+        long result = 1;
+        int count = 0;
+        while (count < numberB)
+        {
+            result *= numberA;
+            count++;
+        }
 
-Console.WriteLine($"{numberA}^{numberB}={result}");
+        Console.WriteLine($"{numberA}^{numberB}={result}");
+    }
 }
-
 
 
 void Solution2()
 
 {
-Console.WriteLine("Insert number A:");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Insert number B:");
-int numberB = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Insert number A:");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Insert number B:");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    if (numberB < 0)
+    {
+        Console.WriteLine("The number B does't natural");
+    }
+    else
+    {
+    long result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result *= numberA;
+    }
 
-long result = 1;
-for (int i = 1; i <= numberB; i++)
-{
-    result *= numberA;
+    Console.WriteLine($"{numberA}^{numberB}={result}");
 }
-
-Console.WriteLine($"{numberA}^{numberB}={result}");
 }
-
 Solution1();
 Solution2();
